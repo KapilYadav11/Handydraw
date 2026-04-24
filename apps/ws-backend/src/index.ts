930f450a17b9feeda6e7aac790b1d0949cf1c8df
KapilYadav11 <kapilyadav951641@gmail.com>
2026-04-24 22:13:22 +0530

    Kvvkfjd

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,10 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080});
+
+wss.on('connection', function connction(ws){
+
+    ws.on('message', function message(data){
+        ws.send('pong');
+    })
+})
